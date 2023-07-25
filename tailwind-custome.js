97008aef74379c452c9cc65424597562fcd41ccb
akibrahman <akibrahman5200@gmail.com>
2023-07-25 17:33:35 +0600

    Navigationbar and Hero Section Completed

---
+++ tailwind-custome.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        // main: "#B68C5A",
+      },
+      fontFamily: {
+        // main: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        hero: "url('./images/yellow-bg.jpg')",
+      },
+    },
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{html,js}"],
   plugins: [],
-}
+};
